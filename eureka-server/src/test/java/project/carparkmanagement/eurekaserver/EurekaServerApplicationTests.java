5c4a42473c631f2d344c5ae58bf2b710b77ff2a4
mesut <guzelkaralarm@gmail.com>
2023-11-01 12:21:33 +0300

    - Add api-gateway

--- eureka-server/src/test/java/project/carparkmanagement/eurekaserver/EurekaServerApplicationTests.java
+++ eureka-server/src/test/java/project/carparkmanagement/eurekaserver/EurekaServerApplicationTests.java
@@ -1,10 +1,10 @@
-package project.carparkmanagement.carparkservice.eurekaservice;
+package project.carparkmanagement.eurekaserver;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class EurekaServiceApplicationTests {
+class EurekaServerApplicationTests {
 
     @Test
     void contextLoads() {
